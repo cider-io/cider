14b68fe9bc51445b532af0cc4b71177f63997303
Roshini Saravanakumar <34928068+roshinis78@users.noreply.github.com>
2021-03-11 13:29:28 -0800

    Gossip: Initial membership list and getMyIp

--- cider.go
+++ cider.go
@@ -3,10 +3,12 @@ package main
 import (
 	"cider/sysinfo"
 	"cider/log"
+	"cider/gossip"
 )
 
 func main() {
-	log.InitLogger(log.ToCiderLog)
+	log.InitLogger(log.ToStdout)
 	sysinfo := sysinfo.SysInfo()
 	log.Logger.Println(sysinfo)
+	gossip.Start()
 }
---
+++ gossip/gossip.go
@@ -0,0 +1,59 @@
+package gossip
+
+import (
+	"cider/log"
+	"net"
+	"regexp"
+	"errors"
+)
+
+type Member struct {
+	Version int
+	Failed bool
+}
+
+var group map[string]Member // maps member IP address to Member struct
+var MyIp string
+
+func Start() {
+	var err error
+	log.Logger.Println("Starting gossip")
+
+	// initial membership list
+	group := make (map[string]Member)
+	MyIp, err = getMyIp()
+	log.HandleError(log.Error, err)
+	group[MyIp] = Member{Reincarnation: 0}
+
+	// TODO: add introducer to the membership list after adding the introducer cli arg 
+	log.Logger.Println("Inital membership list:", group)
+}
+
+func getMyIp() (string, error) {
+	interfaces, err := net.Interfaces()
+	log.HandleError(log.Error, err)
+
+	// TODO: this only covers WLANs, we'd also want to support wired LANs
+	wifiPattern, err := regexp.Compile("(?i:.*(wifi|wi-fi).*)")
+	log.HandleError(log.Error, err)
+
+	for _, iface := range interfaces {
+		interfaceIsUp := net.FlagUp & iface.Flags == net.FlagUp 
+		interfaceIsWifi := wifiPattern.MatchString(iface.Name)
+
+		if interfaceIsUp && interfaceIsWifi {
+			unicastAddresses, err := iface.Addrs()
+			log.HandleError(log.Error, err)
+
+			for _, address := range unicastAddresses {
+				switch value := address.(type) {
+				case *net.IPNet:
+					if value.IP.To4() != nil {
+						return value.IP.String(), nil
+					}
+				}
+			}
+		}
+	}
+	return "", errors.New("Your device is not connected to the WLAN.")
+}
