5f41d387a27159fbb4af7796ef1f59105cda561b
Roshini Saravanakumar <34928068+roshinis78@users.noreply.github.com>
2021-03-13 11:14:38 -0800

    Gossip: Address PR feedback

--- gossip/gossip.go
+++ gossip/gossip.go
@@ -2,53 +2,54 @@ package gossip
 
 import (
 	"cider/log"
+	"cider/util"
+	"cider/config"
 	"net"
-	"regexp"
-	"errors"
 	"encoding/gob"
 	"sync"
 	"time"
 	"strconv"
 )
 
-type Member struct {
+type Member struct { // membership list entry
 	Version int
 	Heartbeat int
 	LastUpdated time.Time
 	Failed bool
 }
 
-var MyView map[string]Member // maps member IP address to Member struct
-var MyIp string
-var infected bool
+type Node struct {
+	IpAddress string
+	MembershipList map[string]Member // maps member IP address to Member struct
+	Infected bool
+}
 
-const gossipPort = 7000
-const heartbeatRate = 1 * time.Second
+var Self Node
 
 // heartbeat: Send the local membership list over UDP
 func heartbeat(neighborIp string) {
 	// update my heartbeat
-	me := MyView[MyIp]
+	me := Self.MembershipList[Self.IpAddress]
 	me.Heartbeat++
-	MyView[MyIp] = me
+	Self.MembershipList[Self.IpAddress] = me
 
-	connection, err := net.Dial("udp", neighborIp + ":" + strconv.Itoa(gossipPort))
+	connection, err := net.Dial("udp", neighborIp + ":" + strconv.Itoa(config.GossipPort))
 	log.HandleError(log.Error, err)
 	encoder := gob.NewEncoder(connection)
-	encoder.Encode(MyView)
+	encoder.Encode(Self.MembershipList)
 }
 
-// listenForGossip: Report incoming gossip to updateView
+// listenForGossip: Report incoming gossip to updateMembershipList
 func listenForGossip() {
-	udpAddress := net.UDPAddr{IP: net.ParseIP(MyIp), Port: gossipPort, Zone: ""}
+	udpAddress := net.UDPAddr{IP: net.ParseIP(Self.IpAddress), Port: config.GossipPort, Zone: ""}
 	udpConnection, err := net.ListenUDP("udp", &udpAddress)
 	log.HandleError(log.Error, err)
 	for {
-		var neighborsView map[string]Member
+		var neighborsMembershipList map[string]Member
 		decoder := gob.NewDecoder(udpConnection)
-		err = decoder.Decode(&neighborsView)
+		err = decoder.Decode(&neighborsMembershipList)
 		log.HandleError(log.Warning, err)
-		updateView(neighborsView)
+		updateMembershipList(neighborsMembershipList)
 	}
 }
 
@@ -56,90 +57,48 @@ func listenForGossip() {
 func gossip() {
 	startTime := time.Now()
 	for {
-		if time.Since(startTime) > heartbeatRate {
-			heartbeat(MyIp)
+		if time.Since(startTime) > config.HeartbeatRate {
+			heartbeat(Self.IpAddress)
 			startTime = time.Now()
 		}
 	}
 }
 
-// TODO: updateView:
-func updateView(neighborsView map[string]Member) {
-	for ip, member := range neighborsView {
+// TODO: updateMembershipList:
+func updateMembershipList(neighborsMembershipList map[string]Member) {
+	for ip, member := range neighborsMembershipList {
 		prettyPrintMember(ip, member)
 	}
 }
 
-// getMyIp: Return this device's IP address on the WLAN
-func getMyIp() (string, error) {
-	interfaces, err := net.Interfaces()
-	log.HandleError(log.Error, err)
-
-	// FIXME: ethernet usually shows up before wifi
-	// user should be able to configure which interface they want CIDER client to run on
-	lanPattern, err := regexp.Compile("(?i:.*(wifi|wi-fi|eth).*)")
-	log.HandleError(log.Error, err)
-
-	for _, iface := range interfaces {
-		interfaceIsUp := net.FlagUp & iface.Flags == net.FlagUp 
-		interfaceIsLan := lanPattern.MatchString(iface.Name)
-
-		if interfaceIsUp && interfaceIsLan {
-			unicastAddresses, err := iface.Addrs()
-			log.HandleError(log.Error, err)
-
-			for _, address := range unicastAddresses {
-				switch value := address.(type) {
-				case *net.IPNet:
-					if value.IP.To4() != nil {
-						return value.IP.String(), nil
-					}
-				}
-			}
-		}
-	}
-	return "", errors.New("Your device is not connected to the LAN.")
-}
-
-func prettyPrintMember(ip string, member Member) {
-	summary := "[" + ip + "-" + strconv.Itoa(member.Version) + "]"
-	summary += " [♥:" + strconv.Itoa(member.Heartbeat) + "]"
-	summary += " [Last updated " + strconv.FormatInt(time.Since(member.LastUpdated).Milliseconds(), 10) + " ago]"
-	if member.Failed {
-		summary += " [FAILED]"
-	}
-	log.Logger.Println(summary)
-}
-
 // Start: Run gossip for group membership and failure detection
 func Start() {
-	var err error
 	var wg sync.WaitGroup
 	wg.Add(1)
 
-	log.Logger.Println("Starting gossip")
-
-	infected = false // the node is initially uninfected
-
-	// initial membership list
-	MyView = make(map[string]Member)
-	MyIp, err = getMyIp()
+	// initialize node
+	ipAddress, err := util.GetIpAddress()
 	log.HandleError(log.Error, err)
-	MyView[MyIp] = Member{Version: 0, Heartbeat: 0, LastUpdated: time.Now(), Failed: false}
-
+	membershipList := make(map[string]Member)
 	// TODO: add introducer to the membership list after adding the introducer cli arg 
-	log.Logger.Println("Inital membership list:", MyView)
+	membershipList[ipAddress] = Member{Version: 0, Heartbeat: 0, LastUpdated: time.Now(), Failed: false}
+	Self = Node{IpAddress: ipAddress, MembershipList: membershipList, Infected: false}
+	
+	prettyPrintNode("Initial node configuration:", Self)
+
+	log.Logger.Println("Starting gossip")
 
 	go func() {
 		listenForGossip()
 		wg.Done()
 	}()
-	
+
 	go func() {
 		gossip()
 		wg.Done()
 	}()
 
 	wg.Wait()
+	log.Logger.Fatalln("Gossip has exited")
 }
 
---
+++ util/util.go
@@ -0,0 +1,39 @@
+package util 
+
+import (
+	"net"
+	"regexp"
+	"cider/log"
+	"errors"
+)
+
+// GetIpAddress: Return this device's IP address on the WLAN
+func GetIpAddress() (string, error) {
+	interfaces, err := net.Interfaces()
+	log.HandleError(log.Error, err)
+
+	// FIXME: ethernet usually shows up before wifi
+	// user should be able to configure which interface they want CIDER client to run on
+	lanPattern, err := regexp.Compile("(?i:.*(wifi|wi-fi|eth).*)")
+	log.HandleError(log.Error, err)
+
+	for _, iface := range interfaces {
+		interfaceIsUp := net.FlagUp & iface.Flags == net.FlagUp 
+		interfaceIsLan := lanPattern.MatchString(iface.Name)
+
+		if interfaceIsUp && interfaceIsLan {
+			unicastAddresses, err := iface.Addrs()
+			log.HandleError(log.Error, err)
+
+			for _, address := range unicastAddresses {
+				switch value := address.(type) {
+				case *net.IPNet:
+					if value.IP.To4() != nil {
+						return value.IP.String(), nil
+					}
+				}
+			}
+		}
+	}
+	return "", errors.New("Your device is not connected to the LAN.")
+}
